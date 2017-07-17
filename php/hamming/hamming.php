577c9c14125c72546e47ab67aad0198833a68645
SyncroIT <kochylo.paolo@gmail.com>
2017-07-17 14:14:34 +0200

    PHP exercises

---
+++ php/hamming/hamming.php
@@ -0,0 +1,16 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+function distance($a, $b)
+{
+	$diff = 0;
+	if(strlen($a) !== strlen($b)) throw new Exception('DNA strands must be of equal length.', 'InvalidArgumentException');
+	for($i = 0; $i < strlen($a); $i++) {
+		if($a[$i] !== $b[$i]) $diff++;
+	}
+	return $diff; 
+}
